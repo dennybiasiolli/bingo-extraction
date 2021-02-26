7caebeb65b511655f4c2e68149c99d3eb6043f33
Denny Biasiolli <denny.biasiolli@gmail.com>
2021-02-26 17:09:19 +0100

    components: adding DisplayNumbers component

---
+++ src/components/DisplayNumbers.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-card elevation="2">
+    <v-card-title>{{ title }}</v-card-title>
+    <v-card-text>
+      <v-chip v-for="n of numbers" :key="n" class="ma-1">{{ n }}</v-chip>
+    </v-card-text>
+  </v-card>
+</template>
+
+<script>
+export default {
+  name: 'DisplayNumbers',
+  props: {
+    title: {
+      type: String,
+      required: true,
+    },
+    numbers: {
+      type: Array,
+      required: true,
+    },
+  },
+};
+</script>
