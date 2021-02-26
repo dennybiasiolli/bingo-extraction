ace8767a840f4a1d3f3d0beb6c7ea383f5c2d7bd
Denny Biasiolli <dennybiasiolli@users.noreply.github.com>
2021-02-26 17:48:01 +0100

    views(Home): adding extraction of a number

--- tests/unit/views/Home.spec.js
+++ tests/unit/views/Home.spec.js
@@ -2,10 +2,26 @@ import { shallowMount } from '@vue/test-utils';
 import Home from '@/views/Home.vue';
 
 describe('Home.vue', () => {
+  const shallowMountComponent = () => shallowMount(Home, {
+    stubs: ['v-container', 'v-btn', 'v-row', 'v-col'],
+  });
+
   it('renders as expected', () => {
-    const wrapper = shallowMount(Home, {
-      stubs: ['v-container'],
-    });
+    const wrapper = shallowMountComponent();
     expect(wrapper).toMatchSnapshot();
+  });
+
+  it('extracts a number and render as expected', async () => {
+    jest.spyOn(global.Math, 'random')
+      .mockReturnValueOnce(0.123456789)
+      .mockReturnValueOnce(0.987654321);
+    const wrapper = shallowMountComponent();
+    wrapper.vm.handleExtract();
+    await wrapper.vm.$nextTick();
+    expect(wrapper).toMatchSnapshot();
+    wrapper.vm.handleExtract();
+    await wrapper.vm.$nextTick();
+    expect(wrapper).toMatchSnapshot();
+    jest.spyOn(global.Math, 'random').mockRestore();
   });
 });
