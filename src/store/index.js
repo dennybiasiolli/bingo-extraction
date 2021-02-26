60b6336e044c8de67149c95e0c9325f597dce770
Denny Biasiolli <denny.biasiolli@gmail.com>
2021-02-26 19:14:35 +0100

    store: adding the same Home logic into the store

--- src/store/index.js
+++ src/store/index.js
@@ -3,13 +3,36 @@ import Vuex from 'vuex';
 
 Vue.use(Vuex);
 
+export const defaultState = {
+  availableNumbers: [...Array(90).keys()].map((i) => i + 1),
+  extractedNumbers: [],
+};
+
+export const getters = {
+  ascendingExtractedNumbers(state) {
+    return [...state.extractedNumbers].sort((a, b) => a - b);
+  },
+  reversedExtractedNumbers(state) {
+    return [...state.extractedNumbers].reverse();
+  },
+};
+
+export const mutations = {
+  extractNumber(state) {
+    const index = Math.floor(Math.random() * state.availableNumbers.length);
+    const extracted = state.availableNumbers.splice(index, 1);
+    state.extractedNumbers = state.extractedNumbers.concat(extracted);
+  },
+};
+
+export const actions = {};
+
+export const modules = {};
+
 export default new Vuex.Store({
-  state: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  },
+  state: defaultState,
+  getters,
+  mutations,
+  actions,
+  modules,
 });
